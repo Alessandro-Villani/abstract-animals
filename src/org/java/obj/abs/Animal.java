ff206d6e7c548e0813be236f7ff7b8eafccd4925
Alessandro-Villani <alessvill@libero.it>
2023-05-11 12:13:21 +0200

    added Animal class

--- src/org/java/obj/abs/Animal.java
+++ src/org/java/obj/abs/Animal.java
@@ -7,6 +7,27 @@ public abstract class Animal {
 	public Animal(String name) {
 		
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void dormi() {
+		System.out.println("Zzz");
+	}
+	
+	public abstract void verso();
+	
+	public abstract void mangia();
+	
+	@Override
+	public String toString() {
+		return name;
+	}
 	
 	
 
